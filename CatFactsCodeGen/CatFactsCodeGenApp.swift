ada64593987bfb4e1471aecd82425437410f86b5
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-08-06 15:03:39 +0300

    Initial Commit

---
+++ CatFactsCodeGen/CatFactsCodeGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactsCodeGenApp.swift
+//  CatFactsCodeGen
+//
+//  Created by Tatiana Lazarenko on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsCodeGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatFactsCodeGen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatFactsCodeGen
+//
+//  Created by Tatiana Lazarenko on 8/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
